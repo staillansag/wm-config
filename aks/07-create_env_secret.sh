1648d0f86425fe140e806f8f93165467016e4b59
staillansag <103417302+staillansag@users.noreply.github.com>
2023-02-08 15:53:38 +0100

    Adding wmio playground credentials and Kafka management

--- aks/07-create_env_secret.sh
+++ aks/07-create_env_secret.sh
@@ -8,6 +8,8 @@ kubectl create secret generic environment-secrets \
 	 --from-literal=databaseUser=${DB_USER} \
 	 --from-literal=databasePassword=${DB_PASSWORD} \
          --from-literal=wmioIntegrationUser=${IO_INT_USER} \
+         --from-literal=wmioIntegrationPlaygroundUser=${IO_INT_PLAYGROUND_USER} \
          --from-literal=wmioIntegrationPassword=${IO_INT_PASSWORD} \
+         --from-literal=wmioIntegrationPlaygroundUser=${IO_INT_PLAYGROUND_PASSWORD} \
          --from-literal=apiGatewayUser=${API_GATEWAY_USER} \
          --from-literal=apiGatewayPassword=${API_GATEWAY_PASSWORD}
--- aks/08-create_env_configmap.sh
+++ aks/08-create_env_configmap.sh
@@ -7,6 +7,8 @@ echo "Creating environment specific Kubernetes config map"
 kubectl create configmap environment-config \
 	 --from-literal=apiGatewayUrl=${API_GATEWAY_URL} \
 	 --from-literal=wmioIntegrationUrl=${WMIO_INT_URL} \
+	 --from-literal=wmioIntegrationPlaygroundUrl=${WMIO_INT_PLAYGROUNG_URL} \
+	 --from-literal=kafkaServerList=${KAFKA_SERVER_LIST} \
          --from-literal=domainName=${DOMAIN_NAME} \
          --from-literal=databaseServerName=${DB_SERVERNAME} \
          --from-literal=databaseServerPort=${DB_PORT} \
