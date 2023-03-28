4b11269b24c245d9635f1224101aca85793c725e
staillansag <103417302+staillansag@users.noreply.github.com>
2023-03-28 10:07:51 +0200

    Fixing environment config issues

--- aks/08-create_env_configmap.sh
+++ aks/08-create_env_configmap.sh
@@ -8,3 +8,12 @@ kubectl create configmap global-config \
 	 --from-literal=elasticUrl=${FLUENT_ELASTICSEARCH_HOST} \
 	 --from-literal=elasticPort=${FLUENT_ELASTICSEARCH_PORT} \
 	 --from-literal=elasticScheme=${FLUENT_ELASTICSEARCH_SCHEME}  || exit 1
+
+echo "Creating environment specific Kubernetes config map"
+kubectl create configmap environment-config \
+	 --from-literal=apiGatewayUrl=${API_GATEWAY_URL} \
+	 --from-literal=wmioIntegrationUrl=${WMIO_INT_URL} \
+         --from-literal=domainName=${DOMAIN_NAME} \
+         --from-literal=databaseServerName=${DB_SERVERNAME} \
+         --from-literal=databaseServerPort=${DB_PORT} \
+         --from-literal=databaseName=${DB_NAME}
