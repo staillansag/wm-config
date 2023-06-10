f6265f097505f5f701d435c019f43bef7576b016
staillansag <103417302+staillansag@users.noreply.github.com>
2023-06-10 08:51:08 +0200

    Refactoring

--- aks/07-create_env_secret.sh
+++ aks/07-create_env_secret.sh
@@ -7,12 +7,3 @@ echo "Creating global Kubernetes secrets"
 kubectl create secret generic global-secrets \
 	 --from-literal=elasticUser=${FLUENT_ELASTICSEARCH_USER} \
 	 --from-literal=elasticPassword=${FLUENT_ELASTICSEARCH_PASSWORD}  || exit 1
-
-echo "Creating environment specific Kubernetes secrets"
-kubectl create secret generic environment-secrets \
-	 --from-literal=databaseUser=${DB_USER} \
-	 --from-literal=databasePassword=${DB_PASSWORD} \
-         --from-literal=wmioIntegrationUser=${IO_INT_USER} \
-         --from-literal=wmioIntegrationPassword=${IO_INT_PASSWORD} \
-         --from-literal=apiGatewayUser=${API_GATEWAY_USER} \
-         --from-literal=apiGatewayPassword=${API_GATEWAY_PASSWORD}
