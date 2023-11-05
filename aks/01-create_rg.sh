3dfb7e297230b923ca3d73a23f2a8d1910e1912b
staillansag <stt.lab@outlook.com>
2023-11-05 08:32:26 +0100

    .

--- aks/01-create_rg.sh
+++ aks/01-create_rg.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Creating resource group ${AKS_RESOURCE_GROUP} in region ${AZ_LOCATION}"
-az group create --location ${AZ_LOCATION} \
-	--name ${AKS_RESOURCE_GROUP}  || exit 1
+if [ "${AZ_SANDBOX_MODE}" -eq "true" ]; then
+	AKS_RESOURCE_GROUP=$(az group list --query "[0].name" --output tsv)
+    echo "Running in an Azure sandox, using default resource group ${AKS_RESOURCE_GROUP} in region ${AZ_LOCATION}"
+	echo "##vso[task.setvariable variable=AKS_RESOURCE_GROUP;]${AKS_RESOURCE_GROUP}"
+else
+	echo "Creating resource group ${AKS_RESOURCE_GROUP} in region ${AZ_LOCATION}"
+	az group create --location ${AZ_LOCATION} \
+		--name ${AKS_RESOURCE_GROUP}  || exit 1
+fi
+
