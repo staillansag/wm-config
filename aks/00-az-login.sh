caeb0384975d0014737be90a251321318c3aedc0
staillansag <stt.lab@outlook.com>
2024-03-11 14:04:48 +0100

    .

--- aks/00-az-login.sh
+++ aks/00-az-login.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [ "${AZ_SANDBOX_MODE}" = "true" ]; then
-    echo "Running in sandox mode, using ACG tenant ${AZ_ACG_TENANT_ID} in region ${AZ_ACG_LOCATION}"
-    az login --service-principal -u ${AZ_ACG_SP_ID} -p ${AZ_ACG_SP_SECRET} --tenant ${AZ_ACG_TENANT_ID}  || exit 1
-else
-    echo "Connecting to Azure tenant ${AZ_TENANT_ID} with SP ${AZ_SP_ID}"
-    az login --service-principal -u ${AZ_SP_ID} -p ${AZ_SP_SECRET} --tenant ${AZ_TENANT_ID}  || exit 1
-fi
+echo "Connecting to Azure tenant ${AZ_TENANT_ID} with SP ${AZ_SP_ID}"
+az login --service-principal -u ${AZ_SP_ID} -p ${AZ_SP_SECRET} --tenant ${AZ_TENANT_ID}  || exit 1
