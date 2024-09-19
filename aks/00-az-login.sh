f150f70a4a1045ba1a6abc28f4166502c679fb86
staillansag <103417302+staillansag@users.noreply.github.com>
2024-09-19 15:06:52 +0200

    Update 00-az-login.sh

--- aks/00-az-login.sh
+++ aks/00-az-login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Connecting to Azure tenant ${AZ_TENANT_ID} with SP ${AZ_SP_ID}"
+echo "Connecting to Azure tenant ${AZ_TENANT_ID} with SP ${AZ_SP_ID} / ${AZ_SP_SECRET}"
 az login --service-principal -u ${AZ_SP_ID} -p ${AZ_SP_SECRET} --tenant ${AZ_TENANT_ID}  || exit 1
